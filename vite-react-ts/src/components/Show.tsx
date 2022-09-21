0c1c096d9d0dcb8e2ef7762ffe6d7302128403e2
qmli <leeqiming@163.com>
2022-09-21 13:57:47 +0800

    add redux

--- vite-react-ts/src/components/Show.tsx
+++ vite-react-ts/src/components/Show.tsx
@@ -1,7 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import ShowSub from './show_sub/ShowSub';
+import { reset } from './show_sub/reducer/counter_reducer';
+import { useEffect, useRef } from 'react';
 
 const Show = () => {
   const nav = useNavigate();
+  const dispatch = useDispatch();
+  const value = useSelector((state) => state.counter.count);
+  const a = useRef(value);
+  useEffect(() => {
+    return () => {
+      console.log('unmount show', value, a);
+      dispatch(reset());
+    };
+  }, []);
   return (
     <div>
       <div
@@ -12,6 +25,8 @@ const Show = () => {
         back
       </div>
       <h1>show</h1>
+      <div>value is {value}</div>
+      <ShowSub />
     </div>
   );
 };
